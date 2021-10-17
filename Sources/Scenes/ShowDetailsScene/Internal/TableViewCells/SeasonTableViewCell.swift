44bd4a09c88a020e7c628667c7f2656a33bf044b
Roman Madyanov <romantaken@gmail.com>
2021-10-17 18:41:58 +0300

    Implement SeasonTableViewCell

--- Sources/Scenes/ShowDetailsScene/Internal/TableViewCells/SeasonTableViewCell.swift
+++ Sources/Scenes/ShowDetailsScene/Internal/TableViewCells/SeasonTableViewCell.swift
@@ -1,73 +1,64 @@
-//import UIKit
-//
-//protocol SeasonTableViewCellDelegate: AnyObject
-//{
-//    func didTapViewButton(in cell: SeasonTableViewCell)
-//    func didTapUnseeButton(in cell: SeasonTableViewCell)
-//}
-//
-//final class SeasonTableViewCell: UITableViewCell
-//{
-//    weak var delegate: SeasonTableViewCellDelegate?
-//
-//    private(set) var season: Season?
-//
-//    private lazy var stackView: UIStackView = {
-//        let stackView = UIStackView()
-//        stackView.translatesAutoresizingMaskIntoConstraints = false
-//        stackView.spacing = .standardSpacing * 2
-//        return stackView
-//    }()
-//
-//    private lazy var viewButton: ViewButton = {
-//        let viewButton = ViewButton()
-//        viewButton.delegate = self
-//        return viewButton
-//    }()
-//
-//    private lazy var progressView = ProgressView()
-//
-//    override init(style: CellStyle, reuseIdentifier: String?) {
-//        super.init(style: style, reuseIdentifier: reuseIdentifier)
-//
-//        contentView.addSubview(stackView)
-//        stackView.addArrangedSubview(viewButton)
-//        stackView.addArrangedSubview(progressView)
-//
-//        stackView.snap(insets: UIEdgeInsets(dx: .standardSpacing * 3, dy: .standardSpacing * 2))
-//    }
-//
-//    func setModel(_ season: Season?, show: Show?, animated: Bool = false) {
-//        self.season = season
-//
-//        viewButton.isViewed = season?.progress == 1
-//
-//        guard let season = season else {
-//            progressView.setProgress(0, animated: animated)
-//            progressView.leadingLabelText = nil
-//            progressView.trailingLabelText = nil
-//            return
-//        }
-//
-//        let seasonName = season.name ?? "Season %d".localized(comment: "Default season name", season.number)
-//        progressView.leadingLabelText = "\(season.number). \(seasonName)"
-//        progressView.trailingLabelText = "\(season.numberOfViewedEpisodes)/\(season.numberOfEpisodes)"
-//        progressView.setProgress(season.progress, animated: animated)
-//    }
-//
-//    @available(*, unavailable)
-//    required init?(coder aDecoder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//}
-//
-//extension SeasonTableViewCell: ViewButtonDelegate
-//{
-//    func didTapViewButton(in viewButton: ViewButton) {
-//        delegate?.didTapViewButton(in: self)
-//    }
-//
-//    func didTapUnseeButton(in viewButton: ViewButton) {
-//        delegate?.didTapUnseeButton(in: self)
-//    }
-//}
+import UIKit
+import ConstraintLayout
+import Styling
+
+final class SeasonTableViewCell: UITableViewCell {
+    struct Season {
+        let progress: Float
+        let number: Int
+        let name: String?
+        let numberOfViewedEpisodes: Int
+        let numberOfEpisodes: Int
+    }
+
+    var viewButtonTapHandler: ((ViewButton.State) -> Void)? {
+        didSet { updateUI() }
+    }
+
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.spacing = .standardSpacing * 2
+        return stackView
+    }()
+
+    private lazy var viewButton = ViewButton()
+    private lazy var progressView = ProgressView()
+
+    override init(style: CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        contentView.addSubview(stackView)
+        stackView.addArrangedSubview(viewButton)
+        stackView.addArrangedSubview(progressView)
+
+        stackView.pin(edges: .vertical(.standardSpacing * 2) + .horizontal(.standardSpacing * 3))
+
+        updateUI()
+    }
+
+    func setSeason(_ season: Season?, animated: Bool = false) {
+        viewButton.state = (season?.progress).map { .viewed($0 == 1) } ?? .hidden
+
+        guard let season = season else {
+            progressView.setProgress(0, animated: animated)
+            progressView.showLabels = false
+            return
+        }
+
+        let seasonName = season.name ?? "Season %d".localized(comment: "Default season name", season.number)
+        progressView.leadingLabelText = "\(season.number). \(seasonName)"
+        progressView.trailingLabelText = "\(season.numberOfViewedEpisodes)/\(season.numberOfEpisodes)"
+        progressView.setProgress(season.progress, animated: animated)
+    }
+
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension SeasonTableViewCell {
+    func updateUI() {
+        viewButton.tapHandler = viewButtonTapHandler
+    }
+}
